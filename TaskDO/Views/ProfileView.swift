82fcfa57f450f5d10bde20d0cd62664daa0cabee
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-07-26 13:22:44 -0500

    Complete profile page with log out function

--- TaskDO/Views/ProfileView.swift
+++ TaskDO/Views/ProfileView.swift
@@ -6,10 +6,62 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading ...")
+                }
+                
+            }
+            .navigationTitle("Profile")
+        }
+        .onAppear {
+            viewModel.fetchUserData()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        Image("Avatar")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            .frame(width: 125, height: 125)
+            .padding(.bottom, 25)
+        
+        VStack {
+            Text("Hello \(viewModel.user?.name ?? "User")")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
+            Text(viewModel.user?.email ?? "")
+                .font(.subheadline)
+                .padding(.bottom, 15)
+            
+            if let dateJoined = viewModel.user?.joined {
+                Text("Joined at \(Date(timeIntervalSince1970: viewModel.user?.joined ?? 0).formatted(date: .abbreviated, time: .complete))")
+            }
+            
+            Text("Its nice having you here.")
+                .font(.footnote)
+                .foregroundColor(Color(.secondaryLabel))
+                .padding(.top, 100)
+        }
+        
+        Spacer()
+        
+        TLButton(title: "Log Out", background: .red) {
+            viewModel.logOut()
+        }
+        .frame(width: UIScreen.main.bounds.width - 100, height: 40)
+        .padding(.bottom, 25)
     }
 }
 
